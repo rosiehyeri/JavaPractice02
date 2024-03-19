4687de7025b0513ea3e402a5dab0a251e30392cc
하이미디어학원 <@DESKTOP-9M696J9>
2024-03-19 17:59:23 +0900

    3/19

--- src/com/javaex/practice2/Ex18.java
+++ src/com/javaex/practice2/Ex18.java
@@ -1,10 +1,35 @@
 package com.javaex.practice2;
 
+import java.util.Scanner;
+
+//알파벳을 입력 받아 자음 모음을 구분하세요.
+//⚫ switch~case문을 사용합니다.
+//⚫ 영문 소문자 이외의 입력 값은 고려하지 않습니다.
 public class Ex18 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("알파벳 1글자(소문자)를 입력하세요");
+		System.out.print("알파벳: ");
+		
+		int alp = scanner.nextInt();
+		
+		switch(alp) {
+		case 'a' :
+		case 'e' :
+		case 'i' :
+		case 'o' :
+		case 'u' :
+			System.out.println("모음입니다.");
+			break;
+		default :
+			System.out.println("자음입니다.");
+			break;
+		} 
+		
+		
+		scanner.close();
 	}
 
 }
